538ab45379e6af27bd1730e1213479bf85d71a61
KIMBEOMJIN <kimbj0117@gmail.com>
2023-02-13 18:20:40 +0900

    add
    add

---
+++ 2048/2048.js
@@ -0,0 +1,12 @@
+
+const board = [...document.querySelectorAll("td")];
+const timer = document.querySelector(".timer_main");
+const score = document.querySelector(".score_main");
+
+let boxs = [];
+let scr = 0;
+let ENDNUMBER = 2048;
+
+boxs.forEach((box) => {
+    
+})
